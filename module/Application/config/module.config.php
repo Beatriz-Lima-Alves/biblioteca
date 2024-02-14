5945f1e984f317dfcdabb587bd66643bbf62fa09
Beatriz-Lima-Alves <81188686+Beatriz-Lima-Alves@users.noreply.github.com>
2024-02-14 00:13:40 -0300

    tela inicial do sistema

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -46,6 +46,8 @@ return [
         'exception_template'       => 'error/index',
         'template_map' => [
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/header' => __DIR__ . '/../view/layout/header.phtml',
+            'layout/footer' => __DIR__ . '/../view/layout/footer.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
